f6c575ee66e051fbb4a95b6d675c50536226623a
negi84 <vipin@mindrops.com>
2025-01-21 13:54:42 +0530

    Initial Commit

---
+++ src/test/java/com/project/banking_app_application/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.banking_app_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
